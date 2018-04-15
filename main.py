7d7a2359723d30f59080bafb56595255ff7ae182
Jonny McFadden <jmcfadden1528@gmail.com>
2018-04-14 21:48:06 -0500

    finally finished the project...i think

--- main.py
+++ main.py
@@ -26,12 +26,37 @@ class Blog(db.Model):
 
 @app.route("/blog", methods=['POST', 'GET'])
 def display_blog():
-    return render_template("blog.html")
+    #needs to receive info from new_post
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        new_post = Blog(title, body)
+        db.session.add(new_post)
+        db.session.commit()
+
+        all_posts = Blog.query.all()
+        last_post = all_posts[-1]
+        if new_post == last_post:
+            return redirect ('/post_display?id=' + str(new_post.id))
+        else:
+            return render_template("blog.html", all_posts=all_posts)
+        
 
 
 @app.route("/new_post", methods=['POST', 'GET'])
 def create_new_post():
+    #needs to send info to blog
     return render_template("new_post.html")
+
+@app.route("/post_display", methods=['POST', 'GET'])
+def display_post(): #NEED TO FIGURE OUT HOW TO RENDER TEMPLATE WITH CORRECT ID
+    id = request.args.get('id')
+    
+    blog = Blog.query.get(id)
+    blog_title = blog.title
+    blog_body = blog.body
+
+    return render_template("post_display.html", blog_title=blog_title, blog_body=blog_body)
 
 
 
